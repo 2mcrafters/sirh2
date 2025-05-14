3728ee066e7b04cd071226c7e8d41d978722dd90
Med Nour <sosi66581@gmail.com>
2025-05-14 21:13:45 +0100

    cors *

--- sirh-back/config/cors.php
+++ sirh-back/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie','departements/import'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173','http://localhost:8081'], // React dev server
+    'allowed_origins' => ['*'], // React dev server
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
